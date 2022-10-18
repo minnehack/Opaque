6a8e26e3f7a0f9d5f9120e27246bbcd0e33c14df
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-10-18 23:48:29 +0000

    Set up the database in the nix flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,59 @@
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
+    mysqlEnv = pkgs.dockerTools.buildImage {
+          name = "mysql";
+          tag = "latest";
+
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [
+              pkgs.dockerTools.usrBinEnv
+              pkgs.dockerTools.binSh
+              # pkgs.dockerTools.caCertificates
+              pkgs.dockerTools.fakeNss
+              pkgs.gnused
+              pkgs.hostname
+              pkgs.mysql
+              pkgs.bash
+              pkgs.coreutils
+            ];
+            pathsToLink = [ "/bin" ];
+          };
+
+          runAsRoot = ''
+            #!${pkgs.runtimeShell}
+            ${pkgs.dockerTools.shadowSetup}
+
+            groupadd -r mysql
+            useradd -r -g mysql mysql
+
+            mkdir -p /run/mysqld
+            chown mysql:mysql /run/mysqld
+
+            mkdir -p /var/lib/mysql
+            chown mysql:mysql /var/lib/mysql
+
+            mkdir /tmp
+            chmod 777 /tmp
+
+            hostname 127.0.0.1
+            printf '%s\n' '127.0.0.1' localhost >> /etc/hosts
+
+            mysql_install_db --user=mysql --ldata=/var/lib/mysql --basedir="${pkgs.mysql}"
+
+            printf '%s\n%s\n%s\n' 'CREATE DATABASE mh_reg;' \
+                                  "GRANT ALL ON mh_reg.* to 'mysql'@'%' IDENTIFIED BY 'mysql';" \
+                                  'FLUSH PRIVILEGES;' > /init.sql
+
+            chown mysql:mysql /init.sql
+          '';
+          
+          config = {
+            User = "mysql";
+            Cmd = [ "/bin/mysqld" "--init-file=/init.sql" ];
+          };
+      };
   in {
     packages.default = pkgs.rustPlatform.buildRustPackage {
       pname = "Opaque";
@@ -28,8 +81,11 @@
       ];
       shellHook = ''
         export DATABASE_URL=mysql://minnehack:minnehack@localhost/mh_reg_test
+        podman load < ${mysqlEnv}
+		podman container rm $(podman container stop \
+			$(podman ps -a -q --filter ancestor=localhost/mysql:latest))
+        podman run --detach --rm -it -p 3306:3306 localhost/mysql:latest
         '';
     };
   });
 }
-
